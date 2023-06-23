7ff7662287d163ddb3e6cf6589d0e26e1024c338
QiaoJiatong <1479253436@qq.com>
2023-06-23 08:11:21 +0800

    vue kuangjia

---
+++ Vue/p120_template syntax/vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h3>学习Vue：模板语法</h3>
+    <P>{{ message }}</P>
+    <div>{{ rawHtml }}</div>
+    <div v-html="rawHtml"></div>
+    <div v-bind:id="dynamicId"></div>
+    <p>{{ num + 10 }}</p>
+    <p>{{ flag ? "孙猴子" : "傻猴子" }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      message: "学习Vue",
+      rawHtml: "<a href='http://www.itbaizhan.com'>百战</a>",
+      dynamicId: 10001,
+      num: 10,
+      flag: true
+    }
+  }
+}
+
+
+
+</script>
+
---
+++ Vue/p125_Component base/vue-demo/src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>我是单文件组件</h3>
+</template>
+
+
+<script>
+export default {
+    name: "MyComponent"
+}
+</script>
+
+<!-- scoped:如果在style中添加此属性，就代表着当前样式只在当前组件中生效 -->
+<style scoped>
+h3{
+    color: red;
+}
+</style>
